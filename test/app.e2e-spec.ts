135b6a113f1dd5014e93f53be79cdabc86a0ad1c
moonbyeongmin <qudalsrnt3x@gmail.com>
2023-10-22 21:38:37 +0900

    e2e 테스트 작성 - getOne
    테스트 환경도 실제 환경과 맞춰주기 위해선 main.ts에 설정한 파이프도 그대로 테스트 환경에 설정해줘야 한다.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // 아무 데코레이터도 없는 어떠한 필드의 object를 거른다.
+        forbidNonWhitelisted: true, // 정의되지 않은 필드에 대해서 에러 처리
+        transform: true, // 요청으로 받은 값을 자동 형변환 시켜준다.
+      }),
+    );
     await app.init();
   });
 
@@ -44,5 +51,20 @@ describe('AppController (e2e)', () => {
         .delete('/movies')
         .expect(HttpStatus.NOT_FOUND);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(HttpStatus.OK);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(HttpStatus.NOT_FOUND);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
